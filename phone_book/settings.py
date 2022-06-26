d31f2cb25cfa19df2199066cc6aeb66c9633fa9f
Purnadip Manna <purnadipmanna2702@gmail.com>
2022-06-26 21:53:28 +0530

    Setting_key_env

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nds&wkfyf!bqb!)-08k46yp0khnr91c5r38k6r9==p6q0sbm_@'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
